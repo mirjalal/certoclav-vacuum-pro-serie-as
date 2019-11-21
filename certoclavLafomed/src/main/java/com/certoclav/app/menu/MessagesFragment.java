76d29738c870b0563cbc7cd6eea654bbadec6303
Royal <royal.rahimov@certoclav.com>
2019-11-21 13:05:30 +0400

    fully delete unused screensharing sdk

--- certoclavLafomed/src/main/java/com/certoclav/app/menu/MessagesFragment.java
+++ certoclavLafomed/src/main/java/com/certoclav/app/menu/MessagesFragment.java
@@ -2,36 +2,20 @@ package com.certoclav.app.menu;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
-import android.view.WindowManager;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.Toast;
-
-import com.certoclav.app.AppConstants;
 import com.certoclav.app.R;
 import com.certoclav.app.model.Autoclave;
-import com.certoclav.app.model.Log;
 import com.certoclav.app.settings.SettingsEmailActivity;
-import com.teamviewer.sdk.screensharing.api.TVConfigurationID;
-import com.teamviewer.sdk.screensharing.api.TVCreationError;
-import com.teamviewer.sdk.screensharing.api.TVSession;
-import com.teamviewer.sdk.screensharing.api.TVSessionConfiguration;
-import com.teamviewer.sdk.screensharing.api.TVSessionCreationCallback;
-import com.teamviewer.sdk.screensharing.api.TVSessionFactory;
 
 import cn.pedant.SweetAlert.SweetAlertDialog;
-import es.dmoral.toasty.Toasty;
 
 public class MessagesFragment extends Fragment {
-
-
-    private TVSessionConfiguration.Builder config;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -50,16 +34,6 @@ public class MessagesFragment extends Fragment {
                 }
             }
         });
-
-        config = new TVSessionConfiguration.Builder(
-                new TVConfigurationID(AppConstants.TVConfigurationID));
-        if (Autoclave.getInstance().getUser() != null) {
-            config.setServiceCaseName(Autoclave.getInstance().getUser().getEmail());
-        } else {
-            config.setServiceCaseName(getString(R.string.no_user_info));
-        }
-
-
         return rootView;
     }
 
@@ -98,8 +72,6 @@ public class MessagesFragment extends Fragment {
             public void onClick(View v) {
                 try {
                     dialog.dismissWithAnimation();
-                    //                    showStartScreenShareDialog();
-
                     // open teamviewer quick support apk
                     if(!Autoclave.getInstance().isFDAEnabled()) {
                         Intent launchIntent = getContext().getPackageManager().getLaunchIntentForPackage("com.teamviewer.quicksupport.market");
@@ -130,49 +102,6 @@ public class MessagesFragment extends Fragment {
         dialog.show();
     }
 
-    private void startScreenSharing() {
-        TVSessionFactory.createTVSession(getActivity(), AppConstants.TVCongigurationToken,
-                new TVSessionCreationCallback() {
-                    @Override
-                    public void onTVSessionCreationSuccess(TVSession session) {
-                        session.start(config.build());
-                    }
-
-                    @Override
-                    public void onTVSessionCreationFailed(TVCreationError error) {
-                        Log.e("tv_error", error.name());
-                        Toasty.error(getActivity(), getString(R.string.start_screen_share_problem)+error.name(), Toast.LENGTH_SHORT, true).show();
-                    }
-                });
-    }
-
-    private void showStartScreenShareDialog() {
-        final SweetAlertDialog dialog = new SweetAlertDialog(getActivity(), R.layout.dialog_ask_descption, SweetAlertDialog.WARNING_TYPE);
-        dialog.setContentView(R.layout.dialog_ask_descption);
-        dialog.setTitle(R.string.please_choose_one_of_the_following_options);
-        dialog.setCancelable(true);
-        dialog.setCanceledOnTouchOutside(true);
-
-
-        final EditText editText = (EditText) dialog.findViewById(R.id.editTextDescription);
-        Button buttonStart = (Button) dialog.findViewById(R.id.buttonStart);
-        buttonStart.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                dialog.dismissWithAnimation();
-                config.setServiceCaseDescription(editText.getText().toString());
-                startScreenSharing();
-                getActivity().getWindow().setSoftInputMode(
-                        WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN
-                );
-            }
-        });
-
-        dialog.show();
-
-    }
-
     /**
      * Called when the activity is first created.
      */
@@ -181,7 +110,4 @@ public class MessagesFragment extends Fragment {
         super.onCreate(savedInstanceState);
 
     }
-
-
 }
-
